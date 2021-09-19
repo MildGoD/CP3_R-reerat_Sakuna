c9f6d7ff5a5c0ff08d9209921b52a596013eb9d9
MildGoD <90982893+MildGoD@users.noreply.github.com>
2021-09-19 17:27:56 +0700

    Create Exercise4_R-reerat_S.py

---
+++ Exercise4_R-reerat_S.py
@@ -0,0 +1,9 @@
+fe = 10.01
+gb = 44.20
+ics = 38.97
+cp = 45.67
+print("------ Your Score ------")
+print("Foundation English :",fe)
+print("General Business :",gb)
+print("Introduction to Computer Systems :",ics)
+print("Computer Programming :",cp)
