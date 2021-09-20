1e095f7c2f8b4f2ae6f3d192eee766b20f53dee6
MildGoD <90982893+MildGoD@users.noreply.github.com>
2021-09-20 18:43:17 +0700

    Create Exercise5_2_R-reerat_S.py

---
+++ Exercise5_2_R-reerat_S.py
@@ -0,0 +1,6 @@
+print("-----Speed of movement-----")
+s = int(input("Enter Distance(>1Km) :"))
+t = int(input("Enter The time takes to move(>1Hr) :"))
+
+v = s / t
+print("Speed of movement :",v,"Km/hr")
