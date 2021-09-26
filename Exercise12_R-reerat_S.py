820644e65dd57dc68e278807f67aaac5b593fcd7
MildGoD <90982893+MildGoD@users.noreply.github.com>
2021-09-27 01:45:00 +0700

    Create Exercise12_R-reerat_S.py

---
+++ Exercise12_R-reerat_S.py
@@ -0,0 +1,14 @@
+def Num1(x,y):
+    print("Plus :",x+y)
+def Num2(x,y):
+    print("Minus :",x-y)
+def Num3(x,y):
+    print("Multiplied :",x*y)
+def Num4(x,y):
+    print("Divide :",x/y)
+
+
+Num1(99,96)
+Num2(99,96)
+Num3(99,96)
+Num4(99,96)
