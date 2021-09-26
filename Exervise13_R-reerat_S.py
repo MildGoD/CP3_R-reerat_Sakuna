a5b97d31f73ca6c21730bf9a417f88aa92f4a342
MildGoD <90982893+MildGoD@users.noreply.github.com>
2021-09-27 03:07:22 +0700

    Delete Exervise13_R-reerat_S.py

--- Exervise13_R-reerat_S.py
+++
@@ -1,7 +0,0 @@
-toalPrice = int(input("Enter Toal price :"))
-
-def vatCalculate(toalPrice):
-    result = toalPrice + (toalPrice * 7 / 100)
-    return result
-
-print("Vat :",vatCalculate(toalPrice))
